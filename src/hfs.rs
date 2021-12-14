434fda5d5eecfa7a1635d0b532cb887be77050a2
higuruchi <fumiya2324@gmail.com>
2021-12-14 14:16:21 +0900

    initialize run time parameter

---
+++ src/hfs.rs
@@ -0,0 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(
+    name = "hfs",
+    version = "0.0.1",
+    author = "higuruchi",
+)]
+
+pub struct Args {
+    #[clap(short, long)]
+    pub config_path: String,
+}
